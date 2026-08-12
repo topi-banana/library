574cb3665f1bf035469937a98fbf83515fe37093
topi-banana <support@topi-banana.com>
2026-08-13 04:29:50 +0900

    fix(verify): use write_all for full output

--- verify/src/bin/yukicoder-1469-iter.rs
+++ verify/src/bin/yukicoder-1469-iter.rs
@@ -10,7 +10,7 @@ fn main() {
     let stdout = std::io::stdout();
     let mut out = stdout.lock();
     for (&b, _) in s.iter().rle() {
-        out.write(&[b]).unwrap();
+        out.write_all(&[b]).unwrap();
     }
-    out.write(b"\n").unwrap();
+    out.write_all(b"\n").unwrap();
 }
